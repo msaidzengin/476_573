e648ca7b2e2ec7c8c5807d1fffd7aee76c999036
Merve Mallı <mervemalli97@gmail.com>
2020-03-04 00:03:11 +0300

    fixed the checkbuttons

--- application/gui.py
+++ application/gui.py
@@ -9,51 +9,66 @@ root.minsize(600,400);
 root.title("NER GUI FOR TR")
 root.iconbitmap('icon.ico')
 
-input_training_data = [0,0]
-training_algorithm = [0,0]
-include_training_data = [0,0]
-classifier_methods = [0,0]
+
+root.input_training_data = {"1": BooleanVar(), "2": BooleanVar()}
+root.input_training_data["1"].set(False)
+root.input_training_data["2"].set(False)
+
+root.training_algorithm = {"1": BooleanVar(), "2": BooleanVar()}
+root.training_algorithm["1"].set(False)
+root.training_algorithm["2"].set(False)
+
+root.include_training_data = {"1": BooleanVar(), "2": BooleanVar()}
+root.include_training_data["1"].set(False)
+root.include_training_data["2"].set(False)
+
+root.classifier_methods = {"1": BooleanVar(), "2": BooleanVar()}
+root.classifier_methods["1"].set(False)
+root.classifier_methods["2"].set(False)
+
 
 def choose_file():
     file_name = filedialog.askopenfilename(initialdir = "/" , title = "Choose a file", filetype = (("json", "*.json"),))
     label = Label(root, text = "").pack()
+    return file_name
+    
 
-def write():
-    print(training_algorithm[0])
     
     
 tabControl = ttk.Notebook(root)
 
 root.tab1 = ttk.Frame(tabControl)
 tabControl.add(root.tab1, text = "Load Data")
- 
-root.tab2 = ttk.Frame(tabControl)
-tabControl.add(root.tab2, text = "Preprocess")
-
-root.tab3 = ttk.Frame(tabControl)
-tabControl.add(root.tab3, text = "Train")
-
-root.tab4 = ttk.Frame(tabControl)
-tabControl.add(root.tab4, text = "Predict")
-
-tabControl.pack(expand = 1, fill = "both")
-
 
 enter_file = Label(root.tab1, text = "Choose a file to process (json) :").pack()
 file_explorer = Button(root.tab1, text = "Choose a file", command=choose_file).pack()
 
-transform_1 = ttk.Checkbutton(root.tab2, text="Algorithm 1", state="Unchecked", variable=training_algorithm[0], onvalue=1, offvalue=0, command=write ).pack()
-transform_2 = ttk.Checkbutton(root.tab2, text="Algorithm 2", state="Unchecked", variable=training_algorithm[1], onvalue=1, offvalue=0).pack()
+root.tab2 = ttk.Frame(tabControl)
+tabControl.add(root.tab2, text = "Preprocess")
 
-training_1 = ttk.Checkbutton(root.tab3, text = "Training Data 1", state="Unchecked", variable=include_training_data[0], onvalue=1, offvalue=0).pack()
-training_2 = ttk.Checkbutton(root.tab3, text = "Training Data 2", state="Unchecked", variable=include_training_data[1], onvalue=1, offvalue=0).pack()
+root.transform_1 = ttk.Checkbutton(root.tab2, text="Algorithm 1", var=root.training_algorithm["1"]).pack()
+root.transform_2 = ttk.Checkbutton(root.tab2, text="Algorithm 2", var=root.training_algorithm["2"]).pack()
 
-method_1 = ttk.Checkbutton(root.tab4, text = "Method 1", state="Unchecked", variable=classifier_methods[0], onvalue=1, offvalue=0).pack()
-method_2 = ttk.Checkbutton(root.tab4, text = "Method 2", state="Unchecked", variable=classifier_methods[1], onvalue=1, offvalue=0).pack()
+root.tab3 = ttk.Frame(tabControl)
+tabControl.add(root.tab3, text = "Train")
+
+root.training_1 = ttk.Checkbutton(root.tab3, text = "Training Data 1", var=root.include_training_data["1"]).pack()
+root.training_2 = ttk.Checkbutton(root.tab3, text = "Training Data 2", var=root.include_training_data["2"]).pack()
+
+root.tab4 = ttk.Frame(tabControl)
+tabControl.add(root.tab4, text = "Predict")
+
+root.method_1 = ttk.Checkbutton(root.tab4, text = "Method 1", var=root.classifier_methods["1"]).pack()
+vmethod_2 = ttk.Checkbutton(root.tab4, text = "Method 2", var=root.classifier_methods["2"]).pack()
 
 output = Button(root.tab4, text = "Sonuçlar").pack(side=RIGHT)
+
+tabControl.pack(expand = 1, fill = "both")
+
 
 
 
 root.mainloop()
+
+
 
