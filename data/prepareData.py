7b7c71f0c446d0a08d4ed902bee5874024e2e95d
msaidzengin <m.zengin@etu.edu.tr>
2020-03-19 16:22:00 +0300

    data preparing code

---
+++ data/prepareData.py
@@ -0,0 +1,35 @@
+import json
+import string
+
+with open('normYeni.json', encoding='utf-8') as fh:
+    data = json.load(fh)
+
+
+with open('output.json', encoding='utf-8') as fh:
+    output = json.load(fh)
+
+train_data = []
+
+for i in data['data']:
+    idd = i['id']
+    text = i['text']
+
+    outs = []
+
+    for j in output['data']:
+        if j['id'] == idd:
+            outs.append(j)
+
+    for out in outs:
+        train_data.append(out['text'] + " " + out['category'])
+        text = text.replace(out['text'], '')
+        
+    text = text.translate(str.maketrans('', '', string.punctuation)).lower()
+    text = text.split(' ')
+    for word in text:
+        if word != '':
+            train_data.append(word + " 0")
+
+    for a in train_data:
+        print(a)
+    
