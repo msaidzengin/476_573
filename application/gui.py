cb6ba20599a46e2d8a8d47984e37d3dffc8e9a72
msaidzengin <m.zengin@etu.edu.tr>
2020-03-23 01:41:22 +0300

    gui

--- application/gui.py
+++ application/gui.py
@@ -3,19 +3,15 @@ from tkinter import ttk
 from ttkthemes import ThemedTk
 from tkinter import filedialog
 import time
+import json
+import os
 
 root = ThemedTk(theme = "black")
 root.minsize(500,300)
 root.title("NER GUI FOR TR")
 root.iconbitmap('icon.ico')
 
-root.input_training_data = {"1": BooleanVar(), "2": BooleanVar()}
-root.input_training_data["1"].set(False)
-root.input_training_data["2"].set(False)
-
-root.training_algorithm = {"1": BooleanVar(), "2": BooleanVar()}
-root.training_algorithm["1"].set(False)
-root.training_algorithm["2"].set(False)
+subdirs = {}
 
 root.include_training_data = {"1": BooleanVar(), "2": BooleanVar()}
 root.include_training_data["1"].set(False)
@@ -26,9 +22,43 @@ root.classifier_methods["1"].set(False)
 root.classifier_methods["2"].set(False)
 
 def choose_file():
-    file_name = filedialog.askopenfilename(initialdir = "/" , title = "Choose a file", filetype = (("json", "*.json"),))
-    label = ttk.Label(root, text = "").pack()
-    return file_name
+    cwd = os.getcwd()
+    filepath = filedialog.askopenfilename(initialdir = cwd , title = "Choose a file", filetype = (("json", "*.json"),))
+    
+    if filepath == '':
+        return
+
+    fname = filepath.split('/')[-1]
+    dirname = fname.split('.')
+
+    if dirname[-1] != 'json':
+        errorr = ttk.Label(root.tab1, text = "wrong format", style = "S.TLabel").pack(pady = (40,0))
+        return
+
+    with open(filepath, encoding='utf-8') as fh:
+        data = json.load(fh)
+
+    try:
+        temp = data['data'][0]['text']
+    except:
+        errorr = ttk.Label(root.tab1, text = "wrong json format", style = "S.TLabel").pack(pady = (40,0))
+        return
+
+    if not os.path.exists(dirname[0]):
+        os.makedirs(dirname[0])
+
+    with open(dirname[0] + "/data.json", 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    filename = ttk.Label(root.tab1, text = fname + " ✓", style = "S.TLabel").pack(pady = (40,0))
+
+def preprocess():
+    selection = list(Lb1.curselection())
+    if len(selection) == 0:
+        return
+
+    choosenData = subdirs[selection[0]]
+    
     
     
 tabControl = ttk.Notebook(root)
@@ -47,8 +77,22 @@ file_explorer = ttk.Button(root.tab1, text = "Choose a file", command=choose_fil
 root.tab2 = ttk.Frame(tabControl)
 tabControl.add(root.tab2, text = "Preprocess")
 
-root.transform_1 = ttk.Checkbutton(root.tab2, text="Algorithm 1", style = "S.TCheckbutton", var=root.training_algorithm["1"]).pack(pady = (55,0))
-root.transform_2 = ttk.Checkbutton(root.tab2, text="Algorithm 2", style = "S.TCheckbutton", var=root.training_algorithm["2"]).pack(pady = (10,0))
+cwd = os.getcwd()
+"""
+for dataName in next(os.walk('.'))[1]:
+    root.training_algorithm[subcounter] = BooleanVar()
+    root.training_algorithm[subcounter].set(False)
+    root.transform_1 = ttk.Checkbutton(root.tab2, text=dataName, style = "S.TCheckbutton", var=root.training_algorithm[subcounter]).pack(pady = (55,0))
+    subcounter += 1
+"""
+counter = 1
+Lb1 = Listbox(root.tab2)
+for dirName in next(os.walk('.'))[1]:
+    Lb1.insert(counter, dirName)
+    subdirs[counter - 1] = dirName
+    counter += 1
+Lb1.pack()
+preprocessButton = ttk.Button(root.tab2, text = "Tamam", style = "S.TButton", command = preprocess).pack(pady = (35, 10))
 
 root.tab3 = ttk.Frame(tabControl)
 tabControl.add(root.tab3, text = "Train")
